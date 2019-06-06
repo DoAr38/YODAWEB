a3204f725b1d26eed4899a285d74b62ff05a5aef
DoAr38 <dorian.arnaud@solti.fr>
2019-06-06 13:59:20 +0200

    test webview

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-second-webview.webview",
+      "file": "plugins/cordova-plugin-second-webview/www/webViewPlugin.js",
+      "pluginId": "cordova-plugin-second-webview",
+      "clobbers": [
+        "webview"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-second-webview": "2.11.4"
   };
 });
