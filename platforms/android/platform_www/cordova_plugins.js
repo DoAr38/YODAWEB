89604e974cc6c841d00b3f911bf970d92584dd82
DoAr38 <dorian.arnaud@solti.fr>
2019-06-06 14:22:49 +0200

    info config

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -10,7 +10,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-second-webview": "2.11.4"
+    "cordova-plugin-second-webview": "2.11.4",
+    "cordova-plugin-whitelist": "1.3.3"
   };
 });
--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,19 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-second-webview/www/webViewPlugin.js",
-        "id": "cordova-plugin-second-webview.webview",
-        "pluginId": "cordova-plugin-second-webview",
-        "clobbers": [
-            "webview"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-second-webview": "2.11.4"
-}
+{}
 // BOTTOM OF METADATA
 });
